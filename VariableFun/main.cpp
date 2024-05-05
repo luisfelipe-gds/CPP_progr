9e1b22ed9e02711e260c71361e212588b3ceaaf0
lufegarciads <106779152+lufegarciads@users.noreply.github.com>
2024-05-05 15:26:22 -0400

    Create main.cpp

---
+++ VariableFun/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+using namespace std;
+
+int main() {
+
+	int myInt;    //declare variable
+	myInt = 15;  //assignment or initialization
+
+	double myDouble = 3.14159;  //declaring and assigning at the same time
+
+	cout << myInt << endl;
+	cout << myDouble << endl;
+
+
+	return 0;
+}
