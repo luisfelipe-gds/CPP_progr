3bd2ffc7b6335787aa7cf6f1a83394372778e2ad
lufegarciads <106779152+lufegarciads@users.noreply.github.com>
2024-05-06 11:33:40 -0400

    Update main.cpp

--- Fundamentals/OperatorsAndVariables/RelationalFun/main.cpp
+++ Fundamentals/OperatorsAndVariables/RelationalFun/main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+/*
+	>	greater than
+	>=	greater than or equal to
+	<	less than
+	<=	less than or equal to
+	==	equal-to (equality)
+	!=	not-equal-to (equality)
+
+*/
+
+
+int main() {
+	
+	cout << boolalpha; //sticky operator - stays until you write noboolalpha
+
+	int a = 15;
+	int b = 20;
+	bool areEqual = a == b;
+
+	int myAge = 34;
+
+
+	cout << (a < b) << endl;
+	cout << areEqual << endl;
+	cout << "myAge >= 21? " << (myAge >= 21) << endl;
+	
+	return 0;
+}
