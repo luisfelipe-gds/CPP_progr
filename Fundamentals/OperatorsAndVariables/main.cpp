aff2e29c6ba9b5a445a13980a67bc6f7c59ff55c
lufegarciads <106779152+lufegarciads@users.noreply.github.com>
2024-05-06 11:55:05 -0400

    Add files via upload

---
+++ Fundamentals/OperatorsAndVariables/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+	Symbolic constants and naming conventions.
+	Variables should follow camelCasing.
+	Symbolic constants should follow CAPITALIZED_SNAKE_CASING.
+*/
+
+int main() {
+	
+
+	const double MY_PI = 3.14159;
+
+	const string MY_NAME = "Lufe";
+
+	cout << MY_NAME << endl;
+		
+	return 0;
+}
