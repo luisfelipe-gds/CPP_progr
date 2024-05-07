250b6b9d764f1fedef14d4b194d902682ec3f85b
lufegarciads <106779152+lufegarciads@users.noreply.github.com>
2024-05-07 09:46:27 -0400

    Create main.cpp

---
+++ Fundamentals/OperatorsAndVariables/ProjectAverage3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+/* 
+
+    This program prompts the user for 3 real numbers and calculates their mean value.
+
+*/
+
+int main() {
+  
+  double num1;
+  double num2;
+  double num3;
+
+  cout << "Please enter a real number: " << endl;
+  cin >> num1;
+  
+  cout << "Please enter another real number: " << endl;
+  cin >> num2;
+  
+  cout << "Please enter the last real number: " << endl;
+  cin >> num3;
+
+  double average = (num1 + num2 + num3) / 3
+
+  cout << "The average is: " << average << endl;
+  
+  return 0;
+}
